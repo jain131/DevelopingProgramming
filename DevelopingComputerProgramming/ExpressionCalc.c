a1ce374a6cc42664553f076b58b1738c12aa4da8
Nishant Jain <nishantjain@Nishants-Mac-Pro.local>
2014-11-21 17:50:31 -0800

    jijc

--- DevelopingComputerProgramming/ExpressionCalc.c
+++ DevelopingComputerProgramming/ExpressionCalc.c
@@ -1,15 +1,24 @@
+/*!************ CALCULATOR **************
+********** Created By Nishant Jain ***********
+*** Has basic Arithmetic operations and Extra Features ***
+* Extras: Fibonacci Series and Bitwise Operations *
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#inlcude <math.h>
 
-//Function prototype to calculate Fibonacci series
+//Function prototype 
 void Extra();
-void CalcFib(int r);												
+void CalcFib(int r);	
+void Bitwise();											
 
 int main()
 {
   //Welcome Screen
   printf("Welcome to Calculator. \n");							
-  char check;
+  char check='y';
   int r;
   int ch;
   char opt;
@@ -20,15 +29,15 @@ int main()
   //Opening a new file for Logging info. Opening in reading and writing mode
   Log = fopen ("Log.txt", "w+");							
       
-    do
+    while(check!='n')
     { 															
       printf("Please select an Arithmetic operation from the following: \n");			
-      printf("1)Addition \t 2)Subtraction \t 3)Multiplication \t 4)Division \t 5)Extra Features \n ");
+      printf("1)Addition \t 2)Subtraction \t 3)Multiplication \t 4)Division \t 5)Power \t 6)Extra Features \n ");
       
       //Input of operation, A SPACE before %d makes scanf ignore whitespace
       scanf(" %d", &ch);											
       
-      if(ch!=5)
+      if(ch!=6)
       {
         //Asking user if he wants to use saved previous result
         printf("Do you want to use the previous result as one of the operands (Y/N)? \n");
@@ -65,6 +74,7 @@ int main()
           case 2: res=var1-var2; break;
           case 3: res=var1*var2; break;
           case 4: res=var1/var2; break;
+          case 5: res=pow(var1,var2); break;
         }
          
         //Displaying result from file
@@ -80,10 +90,10 @@ int main()
       else Extra(); 									 
       
       //Asking user to continue or exit
-      printf("Continue y/n?  ");								
+      printf("\nContinue y/n?  ");								
       scanf(" %c",&check);										
       
-    }while(check!='n');    				
+    }    				
    
     // Closing opened text file
     fclose(Log);													
@@ -96,36 +106,72 @@ return(0);
 void Extra()												
 { 
   int ch,r;
-  printf("\nSelect from the following options: \n1)Fibonacci Series \n");
+  printf("\nSelect from the following options: \n1)Fibonacci Series \n2)Bitwise Operations \n");
   scanf(" %d",&ch);
   switch(ch)
   {
-    case 1: printf("Enter the range of Fibonacci series: \n "); scanf(" %d",&r); CalcFib(r); break;
+    case 1: printf("\nEnter the range of Fibonacci series: \n "); 
+    		scanf(" %d",&r); CalcFib(r); break;
+    		
+    case 2: Bitwise();		
   }
 }
-
+ 
 
 //! Function to print Fibonacci Series according to input of the range by the user
 void CalcFib(int r)											
 {
   int j=0, k=1, res;
-  printf("FIBONACCI SERIES: ");
+  long int sum=0;
   
+  printf("FIBONACCI SERIES: ");
   //printing first two values.
   printf("%d %d",j,k); 
 
-    for(int i=2;i<r;i++)
-    {
-      res=j+k;
-      j=k;
-      k=res; 
-      printf(" %d",k);
-    }  
-  printf("\n"); 
+  /*!Using increment operator in for loop
+  * Displaying Fibonacci series
+  * Using "+=" operator
+  * Finding Sum of Fibonacci series
+  */
+  
+  for(int i=2;i<r;i++)
+  {
+    res=j+k;
+    j=k;
+    k=res; 
+    printf(" %d",k); 
+    sum+=k;
+  }  
+  printf("\nSum of Fibonacci series= %ld \n", sum); 
 }
 
+//!Function to use bit operations on variables (input from the user)
+void Bitwise()
+{
+  int A, B, C;
+  printf("Type two integers to learn about bitwise operations. A= ");
+  scanf(" %d", &A);
+  printf("\n B= ");
+  scanf(" %d", &B);
+  C= A & B;
+  printf("AND: A & B = %d \n", C);
+  C= A | B;
+  printf("OR: A | B = %d \n", C);
+  C= A ^ B;
+  printf("XOR: A ^ B = %d \n", C);
+  C= ~A;
+  printf("COMPLEMENT: ~A = %d \n", C);
+  C= A <<2;
+  printf("LESFT SHIFT: A <<2 = %d \n", C);
+  C= A >>2;
+  printf("RIGHT SHIFT: A >>2 = %d \n", C);
+
+}
+
+
 /*! Flowchart diagram: 
-* \image FlowchartCalc.png
+* \image html FlowchartCalc.png
 */
+
 
 
